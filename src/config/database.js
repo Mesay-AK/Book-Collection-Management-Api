360df04f0baef1355712ad85847f7b6c208278aa
Mesay-AK <mesabe77@gmail.com>
2024-11-29 20:36:24 +0300

    removing-vulnerablities

--- src/config/database.js
+++ src/config/database.js
@@ -7,10 +7,7 @@ const connectDataBase = async(MONGO_URI) => {
         if (!MONGO_URI) {
       throw new Error("MONGO_URI is not defined in environment variables");
     }
-        const connect = await mongoose.connect(MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology:true,
-        });
+        const connect = await mongoose.connect(MONGO_URI);
 
         console.log(`Mongo Connected : ${connect.connection.host}`);
     }catch(err){
